c967349b2f8ffd606b69636674ce9fa97676d414
ankittripathi <ankittripathi1456@gmail.com>
2023-05-03 17:08:13 +0530

    feat: stored preferred settings in localstorage

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -3,8 +3,63 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import { useEffect } from "react";
 
+const fontSizes = [
+  {
+    title: "Small",
+    value: "12px",
+  },
+  {
+    title: "Medium",
+    value: "16px",
+  },
+  {
+    title: "Large",
+    value: "20px",
+  },
+];
+const animationSpeeds = [
+  {
+    title: "Slow",
+    value: 2,
+  },
+  {
+    title: "Medium",
+    value: 1,
+  },
+  {
+    title: "Fast",
+    value: 0.5,
+  },
+];
+const primaryColors = [
+  "rgb(255, 0, 86)",
+  "rgb(33, 150, 243)",
+  "rgb(255, 193, 7)",
+  "rgb(0, 200, 83)",
+  "rgb(156, 39, 176)",
+];
+
+const themes = [
+  {
+    "--background-color": "#fff",
+    "--background-light": "#fff",
+    "--shadow-color": "rgba(0,0,0,0.2)",
+    "--text-color": "#0A0A0A",
+    "--text-light": "#575757",
+  },
+  {
+    "--background-color": "rgb(29, 29, 29)",
+    "--background-light": "rgb(77, 77, 77)",
+    "--shadow-color": "rgba(0,0,0,0.2)",
+    "--text-color": "#ffffff",
+    "--text-light": "#eceaea",
+  },
+];
+
 const Settings = () => {
-  const [settings, setSettings] = useState({
+  const storedSettings = JSON.parse(localStorage.getItem("settings"));
+
+  const initialSettings = {
     "--background-color": "#fff",
     "--background-light": "#fff",
     "--primary-color": "rgb(255, 0, 86)",
@@ -13,75 +68,22 @@ const Settings = () => {
     "--text-light": "#575757",
     "--font-size": "16px",
     "--animation-speed": 1,
-  });
+  };
 
-  const [theme, setTheme] = useState("light");
-  const [primaryColor, setPrimaryColor] = useState(0);
-  const [fontSize, setFontSize] = useState(1);
-  const [animationSpeed, setAnimationSpeed] = useState(1);
+  const [settings, setSettings] = useState(storedSettings || initialSettings);
 
-  useEffect(() => {
-    const root = document.documentElement;
-
-    const settingsToLoop =
-      JSON.parse(localStorage.getItem("settings")) || settings;
-    for (let key in settingsToLoop) {
-      root.style.setProperty(key, settingsToLoop[key]);
-    }
-  }, [settings]);
-
-  const fontSizes = [
-    {
-      title: "Small",
-      value: "12px",
-    },
-    {
-      title: "Medium",
-      value: "16px",
-    },
-    {
-      title: "Large",
-      value: "20px",
-    },
-  ];
-  const animationSpeeds = [
-    {
-      title: "Slow",
-      value: 2,
-    },
-    {
-      title: "Medium",
-      value: 1,
-    },
-    {
-      title: "Fast",
-      value: 0.5,
-    },
-  ];
-  const primaryColors = [
-    "rgb(255, 0, 86)",
-    "rgb(33, 150, 243)",
-    "rgb(255, 193, 7)",
-    "rgb(0, 200, 83)",
-    "rgb(156, 39, 176)",
-  ];
-
-  const themes = [
-    {
-      "--background-color": "#fff",
-      "--background-light": "#fff",
-      "--shadow-color": "rgba(0,0,0,0.2)",
-      "--text-color": "#0A0A0A",
-      "--text-light": "#575757",
-    },
-    {
-      "--background-color": "rgb(29, 29, 29)",
-      "--background-light": "rgb(77, 77, 77)",
-      "--shadow-color": "rgba(0,0,0,0.2)",
-      "--text-color": "#ffffff",
-      "--text-light": "#eceaea",
-    },
-  ];
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const [primaryColor, setPrimaryColor] = useState(
+    primaryColors.findIndex((color) => color === settings["--primary-color"])
+  );
+  const [fontSize, setFontSize] = useState(
+    fontSizes.findIndex((font) => font.value === settings["--font-size"])
+  );
+  const [animationSpeed, setAnimationSpeed] = useState(
+    animationSpeeds.findIndex(
+      (speed) => speed.value === settings["--animation-speed"]
+    )
+  );
 
   function changeFontSize(i) {
     const _size = fontSizes[i];
@@ -110,6 +112,7 @@ const Settings = () => {
     }
     setSettings(_settings);
     localStorage.setItem("settings", JSON.stringify(_settings));
+    localStorage.setItem("theme", i === 0 ? "light" : "dark");
   }
 
   function changeColor(i) {
@@ -120,6 +123,14 @@ const Settings = () => {
     setSettings(_settings);
     localStorage.setItem("settings", JSON.stringify(_settings));
   }
+
+  useEffect(() => {
+    const root = document.documentElement;
+
+    for (let key in settings) {
+      root.style.setProperty(key, settings[key]);
+    }
+  }, [settings]);
 
   return (
     <div>
